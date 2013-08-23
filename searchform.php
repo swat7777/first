20707a7a25fc02b672f1285d82db2b7965747d1b
swat7777 <stankevich1985@mail.ru>
2013-08-23 16:58:23 +0300

    Revert "1"
    This reverts commit ed32313b5814d0597d067c991fb6e8a702da50e8.

--- searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
- <input class="search-query" type="text" name="s" id="s" placeholder="<?php echo "Поиск"; ?>" />
-</form>
